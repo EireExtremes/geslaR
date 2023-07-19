0be450bf65a662299cb678788c4b78763cdee847
Fernando Mayer <fernandomayer@gmail.com>
2023-07-19 20:37:11 +0100

    Adapt to use the download_gesla function

--- R/run_gesla_app.R
+++ R/run_gesla_app.R
@@ -4,37 +4,65 @@
 ##' @title Run the Gesla app
 ##' @return A shiny interface.
 ##' @author Fernando Mayer
-##' @importFrom cli format_error cli_alert_info cli_progress_step
-##' cli_progress_bar cli_progress_update cli_progress_done
-##' @param overwrite Overwrite the current dataset?
+##' @importFrom cli format_error cli_alert_info cli_progress_step cli_progress_message
 ##' @export
+##' @param overwrite Overwrite?
 run_gesla_app <- function(overwrite = FALSE) {
-  if(!requireNamespace("shiny", quietly = TRUE)) {
-    stop(format_error(c("",
-      "x" = "This function requires the {.pkg shiny} package",
-      "i" = "Consider installing it with {.code install.packages('shiny')}")))
-  }
-  sf <- system.file("shiny", "gesla_dataset", package = "geslaR")
-  if(!dir.exists(sf)) {
-    dir.create(sf)
-  }
-  if(overwrite) {
-    unlink(sf, recursive = TRUE)
-  }
-  cli_alert_info(
-    "To use the app, the whole GESLA dataset must be downloaded locally",
-    wrap = TRUE)
-  cli_alert_info(
-    "The total size of the dataset is about 7GB, and the download time will depend on your internet connection",
-    wrap = TRUE)
-  opt <- utils::menu(c("Yes", "No"), title = "Do you wish to continue?")
-  if(opt == 1L) {
-    cli_progress_bar("Wait while the dataset is downloaded...",
-      total = 6)
-    cli_progress_update()
-    shiny::runApp(system.file("shiny", package = "geslaR"))
-    cli_progress_done()
-  } else {
-    cli_alert_info("No data downloaded.")
-  }
+    if(!requireNamespace("shiny", quietly = TRUE)) {
+        stop(format_error(c("",
+            "x" = "This function requires the {.pkg shiny} package",
+            "i" =
+                "Consider installing it with {.code install.packages('shiny')}"))
+        )
+    }
+    gd <- "inst/shiny/gesla_dataset"
+    if(!dir.exists(gd) && overwrite) {
+        stop(format_error(c("",
+            "x" = "The GESLA dataset was not found locally",
+            "i" =
+                "The argument {.arg overwrite = TRUE} has no effect if the dataset is not available",
+            "i" =
+                "Use {.arg overwrite = FALSE} to download the dataset first"))
+        )
+    }
+    if(!dir.exists(gd) && !overwrite) {
+        cli_alert_info(
+            "To use the app, the whole GESLA dataset must be downloaded locally",
+            wrap = TRUE)
+        cli_alert_info(
+            "The total size of the dataset is about 7GB, and the download time will depend on your internet connection",
+            wrap = TRUE)
+        opt <- utils::menu(c("Yes", "No"), title = "Do you wish to continue?")
+        if(opt == 1L) {
+            cli_alert_info("Wait while the dataset is downloaded...")
+            download_gesla(messages = FALSE, ask = FALSE, overwrite = FALSE)
+        } else {
+            stop(format_error(c("",
+                "x" = "No data was downloaded",
+                "i" =
+                    "The app will only work with the dataset downloaded locally"))
+            )
+        }
+    }
+    if(dir.exists(gd) && overwrite) {
+        cli_alert_info(
+            "You chose {.arg overwrite = TRUE} to download the dataset again",
+            wrap = TRUE)
+        cli_alert_info(
+            "The total size of the dataset is about 7GB, and the download time will depend on your internet connection",
+            wrap = TRUE)
+        opt <- utils::menu(c("Yes", "No"), title = "Do you wish to continue?")
+        if(opt == 1L) {
+            cli_alert_info("Wait while the dataset is downloaded...")
+            download_gesla(messages = FALSE, ask = FALSE, overwrite = TRUE)
+        } else {
+            stop(format_error(c("",
+                "x" = "No data was downloaded",
+                "i" =
+                    "If you don't want to download the dataset again, use {.arg overwrite = FALSE}"))
+            )
+        }
+    }
+    cli_alert_info("Running app...")
+    shiny::runApp(system.file("shiny", package = "geslaR"), quiet = TRUE)
 }
