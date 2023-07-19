d619b5fd3b995b907853eb975a7de2b29047f727
Fernando Mayer <fernandomayer@gmail.com>
2023-07-20 00:47:42 +0100

    Move check_suggests to utils

---
+++ R/utils.R
@@ -0,0 +1,11 @@
+check_suggests <- function() {
+    ## Check if and which suggested packages are installed
+    ## The list of pkgs here are exclusively for the app
+    pkgs <- c("tidyverse", "lubridate", "shiny", "shinyWidgets",
+        "leaflet", "patchwork", "shinycssloaders", "shinyalert",
+        "leafpop", "plotly", "bslib")
+    is_installed <- sapply(pkgs, requireNamespace, quietly = TRUE)
+    installed_pkgs <- names(which(is_installed))
+    not_installed <- setdiff(pkgs, installed_pkgs)
+    return(not_installed)
+}
