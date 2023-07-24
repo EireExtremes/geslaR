f68c1cdf7c8a83fd451c8602df44a6a71a652353
Fernando Mayer <fernandomayer@gmail.com>
2023-07-24 20:21:08 +0100

    More tests

--- tests/testthat/test-run_gesla_app.R
+++ tests/testthat/test-run_gesla_app.R
@@ -26,35 +26,30 @@ test_that("Dataset download prompt works as expected when user declines", {
         "No data was downloaded")
 })
 
-## test_that("Dataset overwrite prompt works as expected", {
-##   # Simulate the situation where the dataset is available
-##   # and the user chooses to overwrite it.
-##   # Set app_dest and dest to a temporary directory
-##   app_dest <- tempfile()
-##   dest <- tempfile()
-##   dir.create(dest)  # Create a mock dataset directory
+test_that("Dataset overwrite prompt works as expected", {
+    ## Simulate the situation where the dataset is available and the
+    ## user chooses to overwrite it
+    app_dest <- tempfile()
+    ## dir.create(paste0(app_dest, "/gesla_dataset"))
+    ## Use mock for utils::menu to simulate user input
+    ## local_mocked_bindings(menu = function(...) 1L, .package = "utils")
+    expect_error(run_gesla_app(app_dest = app_dest, overwrite = TRUE,
+        open = FALSE),
+        "The GESLA dataset was not found locally")
+})
 
-##   # Use mock for utils::menu to simulate user input
-##   with_mock(utils::menu = function(...) 1L) {
-##     expect_message(run_gesla_app(app_dest = app_dest, dest = dest, overwrite = TRUE),
-##                    "Wait while the dataset is downloaded...")
-##   }
-## })
-
-## test_that("Dataset overwrite prompt works as expected when user declines", {
-##   # Simulate the situation where the dataset is available
-##   # and the user chooses not to overwrite it.
-##   # Set app_dest and dest to a temporary directory
-##   app_dest <- tempfile()
-##   dest <- tempfile()
-##   dir.create(dest)  # Create a mock dataset directory
-
-##   # Use mock for utils::menu to simulate user input
-##   with_mock(utils::menu = function(...) 2L) {
-##     expect_error(run_gesla_app(app_dest = app_dest, dest = dest, overwrite = TRUE),
-##                  "No data was downloaded")
-##   }
-## })
+test_that("Dataset overwrite prompt works as expected when user declines", {
+    # Simulate the situation where the dataset is available
+    # and the user chooses not to overwrite it.
+    # Set app_dest and dest to a temporary directory
+    app_dest <- tempdir()
+    dir.create(paste0(app_dest, "/gesla_dataset"))
+    # Use mock for utils::menu to simulate user input
+    local_mocked_bindings(menu = function(...) 1L, .package = "utils")
+    expect_message(run_gesla_app(app_dest = app_dest, overwrite = TRUE,
+        open = FALSE),
+        "You chose `overwrite = TRUE` to download the dataset again")
+})
 
 ## test_that("Shiny app runs successfully", {
 ##   # Simulate the situation where the dataset is available
