dbc88493379a178bafc448b03c43b61e243525f0
Fernando Mayer <fernandomayer@gmail.com>
2023-07-24 19:21:51 +0100

    Tests for run_gesla_app

--- tests/testthat/test-run_gesla_app.R
+++ tests/testthat/test-run_gesla_app.R
@@ -1,40 +1,30 @@
 test_that("Required packages are available", {
-    ## Check if the required packages are available
-    ## missing_pkgs <- c("package1", "package2")  # Replace with the actual
-    ## # missing packages, if known.
+    ## Expect all Suggests packages are available
     missing_pkgs <- check_suggests()
     expect_equal(length(missing_pkgs), 0L,
         info = paste("The following required packages are missing:",
             paste(missing_pkgs, collapse = ", ")))
 })
 
-## test_that("Dataset download prompt works as expected", {
-##   # Simulate the situation where the dataset is not available
-##   # and the user chooses to download it.
-##   # Set app_dest and dest to a temporary directory
-##   app_dest <- tempfile()
-##   dest <- tempfile()
+test_that("Dataset download prompt works as expected", {
+    ## Simulate the situation where the dataset is not available and the
+    ## user chooses to download it
+    app_dest <- tempfile()
+    ## Use mock for utils::menu to simulate user input
+    local_mocked_bindings(menu = function(...) 1L, .package = "utils")
+    expect_message(run_gesla_app(app_dest = app_dest, open = FALSE),
+        "Wait while the dataset is downloaded...")
+})
 
-##   # Use mock for utils::menu to simulate user input
-##   with_mock(utils::menu = function(...) 1L) {
-##     expect_message(run_gesla_app(app_dest = app_dest, dest = dest),
-##                    "Wait while the dataset is downloaded...")
-##   }
-## })
-
-## test_that("Dataset download prompt works as expected when user declines", {
-##   # Simulate the situation where the dataset is not available
-##   # and the user chooses not to download it.
-##   # Set app_dest and dest to a temporary directory
-##   app_dest <- tempfile()
-##   dest <- tempfile()
-
-##   # Use mock for utils::menu to simulate user input
-##   with_mock(utils::menu = function(...) 2L) {
-##     expect_error(run_gesla_app(app_dest = app_dest, dest = dest),
-##                  "No data was downloaded")
-##   }
-## })
+test_that("Dataset download prompt works as expected when user declines", {
+    ## Simulate the situation where the dataset is not available and the
+    ## user chooses not to download it
+    app_dest <- tempfile()
+    ## Use mock for utils::menu to simulate user input
+    local_mocked_bindings(menu = function(...) 2L, .package = "utils")
+    expect_error(run_gesla_app(app_dest = app_dest, open = FALSE),
+        "No data was downloaded")
+})
 
 ## test_that("Dataset overwrite prompt works as expected", {
 ##   # Simulate the situation where the dataset is available
