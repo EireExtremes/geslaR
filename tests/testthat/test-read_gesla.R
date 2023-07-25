4e3d495f5b92ef69986ae2ccc3e4d7a399890fd1
Fernando Mayer <fernandomayer@gmail.com>
2023-07-25 02:05:24 +0100

    Adapt test to read files from testdata

--- tests/testthat/test-read_gesla.R
+++ tests/testthat/test-read_gesla.R
@@ -1,5 +1,41 @@
-test_that("Other file formats", {
+test_that("Reads Parquet file as Arrow Table", {
+    da <- read_gesla(test_path("testdata", "antarctica.parquet"))
+    expect_s3_class(da, "Table")
+})
+
+test_that("Reads Parquet file as tbl_df", {
+    da <- read_gesla(
+        test_path("testdata", "antarctica.parquet"),
+        as_data_frame = TRUE
+    )
+    expect_s3_class(da, "tbl_df")
+})
+
+test_that("Reads CSV file as Arrow Table", {
+    da <- read_gesla(test_path("testdata", "antarctica.csv"))
+    expect_s3_class(da, "Table")
+})
+
+test_that("Reads CSV file as tbl_df", {
+    da <- read_gesla(
+        test_path("testdata", "antarctica.csv"),
+        as_data_frame = TRUE
+    )
+    expect_s3_class(da, "tbl_df")
+})
+
+test_that("Reads CSV file as tbl_df", {
+    da <- read_gesla(
+        test_path("testdata", "antarctica.csv"),
+        as_data_frame = TRUE
+    )
+    expect_s3_class(da, "tbl_df")
+})
+
+test_that("Fails to read other file formats", {
     expect_error({
-        read_gesla(system.file("extdata", "antarctica.txt", package="geslaR"))
+        read_gesla(
+            test_path("testdata", "antarctica.txt")
+        )
     })
 })
