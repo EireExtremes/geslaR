c75c8dc940d015846b341a8929d345afba1cedf6
Fernando Mayer <fernandomayer@gmail.com>
2023-07-26 20:21:22 +0100

    Many improvemnts in query_gesla

--- R/query_gesla.R
+++ R/query_gesla.R
@@ -1,27 +1,96 @@
 ##' @title Query the GESLA dataset
 ##'
-##' @description Query for a subset of the GESLA dataset.
+##' @description This function will make a query to fetch a subset of
+##' the GESLA dataset. At least a country code and one year must be
+##' specified. Site names can also be specified, but are optional. By
+##' default, the resulting subset will contain only data that were
+##' revised and recommended for analysis, by the GESLA group of
+##' researchers.
 ##'
-##' @details Make a query for the GESLA dataset, by selecting one or more
-##' countries and one or more years.
+##' @details The country codes must follow the three-letter [ISO 3166-1
+##' alpha-3](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3) code.
+##' However, note that not all countries are available at the GESLA
+##' dataset. If in doubt, please check the GESLA Shiny app interface
+##' (geslaR-app) online at <https://bit.ly/geslar-app>, or use the
+##' [geslaR::run_gesla_app()] function to open the interface locally.
+##'
+##' The `use_flag` argument must be `1` or `0`, or `c(0, 1)`. The
+##' `use_flag` is a column at the GESLA dataset thet indicates wehter
+##' the data should be used for analysis or not. The `1` (default)
+##' indicates it should, and `0` the otherwise. In a data analysis
+##' scenario, the user must only be interested in using the recommended
+##' data, so this argument shouldn't be changed. However, in same cases,
+##' one must be interested in the non-recommended data, therefore this
+##' option is available. Also, you can specify `c(0, 1)` to fetch all
+##' the data (usable and not usable). In any case, the `use_flag` column
+##' will always be present, and it can be used for any post-processing.
+##' Please, see the [GESLA
+##' format](https://gesla787883612.wordpress.com/format/) documentation
+##' for more details.
+##'
+##' The default argument `as_data_frame = FALSE` will result in an
+##' object of the `arrow_dplyr_query` class. The advantage is that,
+##' regardless of the size of the resulting dataset, the object will be
+##' small in (memory) size. Also, as it happens with the Arrow `Table`
+##' class, it can be manipulated with `dplyr` verbs. Please, see the
+##' documentation at the [Arrow
+##' website](https://arrow.apache.org/docs/r/articles/data_wrangling.html).
+##'
+##' Note that, if the `as_data_frame` argument is set to `TRUE`, the
+##' imported R object will vary in size, according to the size of the
+##' subset. In many situations, this can take a long time an may even be
+##' infeasible, since the object can result in a "larger-than-memory"
+##' size, and possibly will make R operations slow or even a session
+##' crash. Therefore, we always recommend to start with `as_data_frame =
+##' FALSE`, and work with the dataset from there.
 ##'
 ##' @param country A character vector specifying the selected countries,
 ##' using the three-letter [ISO 3166-1
-##' alpha-3](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3) code.
+##' alpha-3](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3) code. See
+##' Details.
 ##' @param year A numeric vector specifying the selected years.
 ##' @param site_name Optional character vector of site names.
-##' @param use_flag See details.
-##' @param as_data_frame Should the query return a data frame (tibble)?
-##' Defaults to FALSE. By default it will return an object of the
-##' `arrow_dplyr_query`.
+##' @param use_flag The default is `1`, which means to use only the data
+##' that was revised and usefull for analysis. Can be `0`, to fetch only
+##' revised and not recommend for analysis, or `c(0, 1)` to fetch all
+##' tha data. See Details.
+##' @param as_data_frame If `FALSE` (default), the data will be imported
+##' as an `arrow_dplyr_query` object. Otherwise, the data will be in a
+##' `tbl_df` (`data.frame`) format. See Details.
 ##'
-##' @return An object of class `arrow_dplyr_query` or a data frame.
+##' @return An object of class `arrow_dplyr_query` or a `tbl_df`
+##' (`data.frame`).
 ##'
 ##' @author Fernando Mayer
 ##'
 ##' @examples
 ##' \dontrun{
-##' da <- query_gesla(country = "ATA", year = 2019)
+##' ## Simple query
+##' da <- query_gesla(country = "ATA", year = 2018)
+##'
+##' ## Multiple years
+##' da <- query_gesla(country = "ATA", year = c(2018, 2019))
+##' da <- query_gesla(country = "ATA", year = 2010:2019)
+##' da <- query_gesla(country = "ATA", year = c(2010, 2012, 2014))
+##' da |>
+##'     count(year) |>
+##'     collect()
+##'
+##' ## Multiple countries
+##' da <- query_gesla(country = c("IRL", "ATA", "BRA"), year = 2018)
+##' da <- query_gesla(country = c("IRL", "ATA", "BRA"), year = 2010:2019)
+##' da |>
+##'     count(country, year) |>
+##'     collect()
+##'
+##' ## Specifying a site name. Note that in this particular case,
+##' ## "Faraday" data is only available for 2018, so no 2019 data will
+##' ## be available
+##' da <- query_gesla(country = "ATA", year = c(2018, 2019),
+##'     site_name = "Faraday")
+##' da |>
+##'     count(year) |>
+##'     collect()
 ##' }
 ##'
 ##' @importFrom dplyr filter collect
@@ -45,6 +114,12 @@ query_gesla <- function(country, year, site_name = NULL, use_flag = 1,
             "x" = "At least one country and one year must be selected."
         )))
     }
+    if(!length(use_flag) %in% c(1, 2) || !any(use_flag %in% c(0, 1))) {
+        stop(format_error(c("",
+            "x" =
+                "{.arg use_flag} must be only 0 or 1, or {.code c(0, 1)}."
+        )))
+    }
     cli_alert_info("This process can take some time, as it depends on the size of the final dataset, and on internet connection.",
                    wrap = TRUE)
     cli_progress_step("Connecting to the data server...")
@@ -57,11 +132,14 @@ query_gesla <- function(country, year, site_name = NULL, use_flag = 1,
     f_daws <- daws |>
         filter(country %in% {{ country }},
             year %in% {{ year }},
-            use_flag == {{ use_flag }})
+            use_flag %in% {{ use_flag }})
     if(!is.null(site_name)) {
         f_daws <- f_daws |>
             filter(site_name %in% {{ site_name }})
     }
+    ## NOTE have to see if this is worthwile, because it will change the
+    ## class to the standard Table. However, it taks some time.
+    ## f_daws <- f_daws |> compute()
     if(as_data_frame) {
         cli_progress_step("Converting to data frame...")
         cli_alert_info("Converting to data frame can take some time and may result in large size objects. Consider using {.arg as_data_frame = FALSE} first.",
--- tests/testthat/test-query_gesla.R
+++ tests/testthat/test-query_gesla.R
@@ -1,6 +1,60 @@
 test_that("S3 connection is available", {
     expect_no_error({
-        query_gesla(country = "IRL", year = 2020, as_data_frame = FALSE)
+        query_gesla(country = "ATA", year = 2018)
     })
     expect_true(arrow_with_s3())
 })
+
+test_that("country and year are missing", {
+    expect_error(query_gesla())
+    expect_error(query_gesla(country = "ATA"))
+    expect_error(query_gesla(year = 2018))
+})
+
+## test_that("S3 connection is available 2", {
+##     local_mocked_bindings(arrow_with_s3 = function(...) FALSE,
+##         .package = "arrow")
+##     expect_error({
+##         query_gesla(country = "ATA", year = 2018)
+##     })
+## })
+
+
+## ## Helper function to check if two data frames are equal
+## assert_data_frame_equal <- function(df1, df2) {
+##   expect_equal(identical(df1, df2), TRUE)
+## }
+
+## test_that("query_gesla returns expected results", {
+##     ## Test Case 1: Basic query with one country and one year
+##     da <- query_gesla(country = "ATA", year = 2018)
+##     expect_s3_class(da, "arrow_dplyr_query")
+
+##     ## Test Case 2: Query with multiple countries and years
+##     da <- query_gesla(country = c("ATA", "IRL"), year = c(2018, 2019))
+##     expect_s3_class(da, "arrow_dplyr_query")
+
+##     ## Test Case 3: Query with site_name specified
+##     da <- query_gesla(country = "ATA", year = c(2018, 2019),
+##         site_name = "Faraday")
+##     expect_s3_class(da, "arrow_dplyr_query")
+
+##     ## Test Case 4: Query with use_flag set to 0
+##     da <- query_gesla(country = "ATA", year = 2018, use_flag = 0)
+##     expect_s3_class(da, "arrow_dplyr_query")
+
+##     ## Test Case 6: Query with use_flag as c(0, 1)
+##     da <- query_gesla(country = "ATA", year = 2018, use_flag = c(0, 1))
+##     expect_s3_class(da, "arrow_dplyr_query")
+
+##   # Test Case 7: Query with invalid use_flag
+##   expect_error(query_gesla(country = "ATA", year = 2019, use_flag = 2))
+## })
+
+## # Additional test to check the equality of the result when as_data_frame is TRUE
+## test_that("query_gesla returns consistent results with as_data_frame = TRUE", {
+##   da_df <- query_gesla(country = "ATA", year = 2019, as_data_frame = TRUE)
+##   da_arrow <- query_gesla(country = "ATA", year = 2019, as_data_frame = FALSE) |> collect()
+
+##   assert_data_frame_equal(da_df, da_arrow)
+## })
